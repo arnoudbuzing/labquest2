fe838425fb7489f873b368b7bcb0c9e7b42c96cc
Arnoud Buzing <arnoudb@wolfram.com>
2017-09-19 15:23:45 -0500

    updates

--- LabQuest2/PacletInfo.m
+++ LabQuest2/PacletInfo.m
@@ -3,8 +3,8 @@ Paclet[
   Description -> "LabQuest2 is a package to use the LabQuest 2 directly from the Wolfram Language",
   Creator -> "Arnoud Buzing",
   Publisher -> "Wolfram Research",
-  Version -> "0.0.0",
-  MathematicaVersion -> "11.2+",
+  Version -> "0.0.1",
+  MathematicaVersion -> "11.1+",
   Extensions -> { 
    { "Documentation", Language -> "English"},
    { "Application", Context -> "LabQuest2`" }
